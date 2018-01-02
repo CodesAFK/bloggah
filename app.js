a4ec45e72552a739c433450f8965bf6d7994a509
Apex-Code <upstaterounder@gmail.com>
2018-01-02 01:25:21 -0600

    update app.js, build out db and setup base application requirements

---
+++ app.js
@@ -0,0 +1,33 @@
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    mongoose   = require('mongoose'),
+    app        = express();
+const server = 1337;
+
+mongoose.connect("mongodb://localhost/bloggah");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+// ============================
+//     SCHEMA                ==
+// ============================
+
+var postSchema = mongoose.Schema({
+   title: String,
+   body:  String,
+   image: String,
+   created: {type: Date, default: Date.now}
+});
+
+var Post = mongoose.model("posts", postSchema);
+
+
+
+// ============================
+//     START SERVER          ==
+// ============================
+
+app.listen(server, function(){
+  console.log("Started Server on " + server);
+});
